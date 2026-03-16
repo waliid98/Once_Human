3d30985c2b4986f0a95dcdb4c32a5acc3aaee387
waliid98 <oualidelaouad98@gmail.com>
2026-03-16 19:20:12 +0000

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -40,8 +40,9 @@ export const VIDEO_LINKS = {
   feature3: "/videos/ved6.mp4",
   feature4: "/videos/ved7.mp4",
   feature5: "/videos/ved7.mp4",
+  export const VIDEOS = {
   hero1: "/videos/ved7.mp4",
   hero2: "/videos/ved2.mp4",
   hero3: "/videos/ved6.mp4",
-  
+  hero4: "/videos/ved4.mp4",
 } as const;
